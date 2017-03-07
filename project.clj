58352a1b5dae450f3549023c3c72ef0f726fca2b
Reijer Copier <Reijer.Copier@kadaster.nl>
2017-03-07 11:55:33 +0100

    Create empty Leiningen project

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject featured-to-geoserver "0.1.0-SNAPSHOT"
+  :description "Open-source GeoServer loading software for Featured"
+  :url "https://github.com/PDOK/featured-to-geoserver"
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
