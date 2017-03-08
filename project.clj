a68a5a59b15b9437db6b41b07c8c9190095a79e5
Reijer Copier <Reijer.Copier@kadaster.nl>
2017-03-08 12:44:31 +0100

    Add empty Ring handler

--- project.clj
+++ project.clj
@@ -2,6 +2,10 @@
   :description "Open-source GeoServer loading software for Featured"
   :url "https://github.com/PDOK/featured-to-geoserver"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [nl.pdok/featured-shared "1.1"]]
+                 [nl.pdok/featured-shared "1.1"]
+                 [ring/ring-defaults "0.2.3"]]
   :target-path "target/%s"
+  :plugins [[lein-ring/lein-ring "0.11.0"]]
+  :ring {:port 5000
+         :handler pdok.featured-to-geoserver.api/app}
   :profiles {:uberjar {:aot :all}})
---
+++ src/pdok/featured_to_geoserver/api.clj
@@ -0,0 +1,6 @@
+(ns pdok.featured-to-geoserver.api)
+
+(defn app [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello World"})
