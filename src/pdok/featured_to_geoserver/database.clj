457302ad256462ce6718d566ff210cb48b28dd2a
Reijer Copier <Reijer.Copier@kadaster.nl>
2017-03-08 11:12:57 +0100

    Implement database access buffering

---
+++ src/pdok/featured_to_geoserver/database.clj
@@ -0,0 +1,32 @@
+(ns pdok.featured-to-geoserver.database)
+
+(defprotocol Transaction
+  (batch-insert [this table records] "Writes a record set to a table.")
+  (commit [this] "Commit transaction"))
+
+(defprotocol Buffering
+  (append-record [this table record] "Append a single record")
+  (commit-buffers [this] "Commit buffer"))
+
+(deftype DefaultBuffering [tx batch-size]
+  Buffering
+  (append-record [this table record]
+    (fn [buffers]
+      (let [buffered-records (or (buffers table) [])
+            new-buffered-records (conj buffered-records record)]
+        (if (< (count new-buffered-records) batch-size)
+          [(assoc buffers table new-buffered-records)]
+          [(dissoc buffers table) [(batch-insert tx table new-buffered-records)]]))))
+  (commit-buffers [this]
+    (fn [buffers]
+      [{} (concat
+            (map (fn [[table records]] (batch-insert tx table records)) buffers)
+            [(commit tx)])])))
+
+(defn process-buffer-operations
+  "Processes a sequence of buffer operations and returns a sequence of the resulting transaction operations"
+  ([buffer-operations] (process-buffer-operations {} buffer-operations))
+  ([buffers buffer-operations]
+    (when-let [buffer-operation (first buffer-operations)]
+      (let [[buffers database-operations] (buffer-operation buffers)]
+        (lazy-seq (concat database-operations (process-buffer-operations buffers (next buffer-operations))))))))
---
+++ test/pdok/featured_to_geoserver/database_test.clj
@@ -0,0 +1,49 @@
+(ns pdok.featured-to-geoserver.database-test
+  (:require [clojure.test :refer :all]
+            [pdok.featured-to-geoserver.database :as database]))
+
+(defn buffering-with-mock-tx
+  ([] (buffering-with-mock-tx 5))
+  ([batch-size]
+    (database/->DefaultBuffering
+      (reify database/Transaction
+        (batch-insert [this table records] [:insert table records])
+        (commit [this] [:commit]))
+      batch-size)))
+
+(deftest test-buffering
+  (is (= 
+        '([:insert :table [{:column "value"}]] [:commit]) 
+        (database/process-buffer-operations
+          (let [b (buffering-with-mock-tx)]
+            (list
+              (database/append-record b :table {:column "value"})
+              (database/commit-buffers b)))))
+      "Should insert a single record and subsequently commit")
+  (is (= 
+        '([:insert :table [{:column-a "value-a"} {:column-b "value-b"} ]] [:commit]) 
+        (database/process-buffer-operations
+          (let [b (buffering-with-mock-tx)]
+            (list
+              (database/append-record b :table {:column-a "value-a"})
+              (database/append-record b :table {:column-b "value-b"})
+              (database/commit-buffers b)))))
+      "Should insert a two record in a single batch and subsequently commit")
+  (is (let [batch-size 5]
+        (=
+          `([:insert :table ~(->> {:column "value"} (repeat) (take batch-size) (into []))] [:commit])
+          (database/process-buffer-operations
+            (let [b (buffering-with-mock-tx batch-size)]
+              (concat
+                (->> (database/append-record b :table {:column "value"}) (repeat) (take batch-size))
+                (list (database/commit-buffers b)))))))
+      "Should insert a single batch of records and subsequently commit")
+  (is (let [batch-size 2] 
+        (=
+          `([:insert :table ~(->> {:column "value"} (repeat) (take batch-size) (into []))] [:insert :table [{:column "value"}]] [:commit])
+          (database/process-buffer-operations
+            (let [b (buffering-with-mock-tx batch-size)]
+              (concat
+                (->> (database/append-record b :table {:column "value"}) (repeat) (take (inc batch-size)))
+                (list (database/commit-buffers b)))))))
+      "Should insert two batches (one containing two records and one containing a single record) and subsequently commit"))
