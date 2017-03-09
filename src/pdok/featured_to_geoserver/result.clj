bce8f0f7f9b4b6a97cce92eaadb47734b68691da
Reijer Copier <Reijer.Copier@kadaster.nl>
2017-03-09 14:13:28 +0100

    Add result monad

---
+++ src/pdok/featured_to_geoserver/result.clj
@@ -0,0 +1,65 @@
+(ns pdok.featured-to-geoserver.result)
+
+(defrecord Result[value error])
+
+(defn- result [x params]
+  (assert
+    (even? (count params)) 
+    "expect an even amount of key-value pairs")
+  (let [keywords (keep-indexed #(if (even? %1) %2) params)] 
+    (assert 
+      (empty? (remove keyword? keywords))
+      "expect only keywords as key in key-value pairs")
+    (assert
+      (empty? (filter #(or (= % :value) (= % :error)) keywords))
+      "non-allowed keyword(s) in params; :value and/or :error encountered"))
+  (if params
+    (apply (partial assoc x) params)
+    x))
+
+(defn unit-result [x & params]
+  (assert x "expect a value, not nil")
+  (result (->Result x nil) params))
+
+(defn error-result [msg & params]
+  (result (->Result nil msg) params))
+
+(defn unit-result? [x]
+  (let [{value :value} x]
+    (some? value)))
+
+(defn error-result? [x]
+  (let [{error :error} x]
+    (some? error)))
+
+(defn bind-result [f r]
+  (let [{value :value error :error} r]
+    (if (some? value)
+      (f value)
+      r)))
+
+(defn filter-result
+  ([f r] (filter-result f "illegal value" r))
+  ([f msg r]
+    (bind-result
+      #(if (f %)
+         (unit-result %)
+         (error-result msg :error-value %))
+      r)))
+
+(defn map-result [f r]
+  (bind-result #(unit-result (f %)) r))
+
+(defmacro ^{:private true} assert-arg [x msg]
+  `(when-not ~x (throw (IllegalArgumentException. (str (first ~'&form) " requires " ~msg " in " *ns* ":" (:line (meta ~'&form)))))))
+
+(defmacro result<- [bindings & body]
+  (assert-arg (vector? bindings) "a vector for its bindings")
+  (assert-arg (even? (count bindings)) "even forms in binding vector")
+  `(let [generator# ~(second bindings)]
+     (bind-result
+       (fn [~(first bindings)]
+         ~(if (= 2 (count bindings))
+            `(unit-result ~@body)
+            `(result<- [~@(drop 2 bindings)] ~@body)))
+       generator#)))
---
+++ test/pdok/featured_to_geoserver/result_test.clj
@@ -0,0 +1,46 @@
+(ns pdok.featured-to-geoserver.result-test
+  (:require [clojure.test :refer :all] 
+            [pdok.featured-to-geoserver.result :refer :all]))
+
+(deftest error-result-test
+  (is (= (->Result nil "problem") (error-result "problem")))
+  (is (= (assoc (->Result nil "problem") :line 42 :col 47) (error-result "problem" :line 42 :col 47))))
+
+(deftest unit-result-test
+  (is (= (->Result "value" nil) (unit-result "value")))
+  (is (= (assoc (->Result "value" nil) :comment "default value") (unit-result "value" :comment "default value")))
+  (is (thrown? AssertionError (unit-result "value" :key-without-value)))
+  (is (thrown? AssertionError (unit-result "value" :key "value" :value "forbidden")))
+  (is (thrown? AssertionError (unit-result "value" :key "value" :error "forbidden"))))
+
+(deftest error-result?-test
+  (is (error-result? (error-result "problem")))
+  (is (not (error-result? (unit-result "value")))))
+
+(deftest unit-result?-test
+  (is (unit-result? (unit-result "ok")))
+  (is (not (unit-result? (error-result "problem")))))
+
+(deftest bind-result-test
+  (is (error-result? (bind-result (fn [_] (error-result "error")) (unit-result 42))))
+  (is (= 47 (:line (bind-result (fn [_] (error-result "error" :line 47)) (unit-result 42))))))
+
+(deftest filter-result-test
+  (is (=
+        (error-result "illegal value" :error-value 42)
+        (filter-result odd? (unit-result 42))))
+  (is (=
+        (error-result "custom message" :error-value 42)
+        (filter-result odd? "custom message" (unit-result 42))))
+  (is (unit-result? (filter-result odd? (unit-result 47)))))
+
+(deftest map-result-test
+  (is (= (unit-result 42) (map-result inc (unit-result 41))))
+  (is (error-result? (map-result inc (error-result "problem")))))
+
+(deftest result<-test
+  (is (= 
+        (unit-result (+ 42 47))
+        (result<- [x (unit-result 42)
+                   y (unit-result 47)]
+                  (+ x y)))))
