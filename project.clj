8eefd3053c9562e594d4946cc6eb8e625e467154
Reijer Copier <Reijer.Copier@kadaster.nl>
2017-03-27 10:20:47 +0200

    Ping request implemented

--- project.clj
+++ project.clj
@@ -4,8 +4,11 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.3.426"]
                  [nl.pdok/featured-shared "1.1"]
+                 [cheshire/cheshire "5.7.0"]
                  [ring/ring-defaults "0.2.3"]
+                 [ring/ring-json "0.4.0"]
                  [org.postgresql/postgresql "9.4.1212.jre7"]
+                 [compojure/compojure "1.5.2"]
                  [clj-time/clj-time "0.13.0"]]
   :target-path "target/%s"
   :plugins [[lein-ring/lein-ring "0.11.0"]
--- src/pdok/featured_to_geoserver/api.clj
+++ src/pdok/featured_to_geoserver/api.clj
@@ -1,6 +1,20 @@
-(ns pdok.featured-to-geoserver.api)
+(ns pdok.featured-to-geoserver.api
+   (:require
+     [ring.middleware.json :refer :all]
+     [ring.util.response :as r]
+     [clj-time [local :as tl]]
+     [compojure.core :refer :all]
+     [compojure.route :as route]))
 
-(defn app [request]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body "Hello World"})
+(extend-protocol cheshire.generate/JSONable
+  org.joda.time.DateTime
+  (to-json [t ^com.fasterxml.jackson.core.JsonGenerator jg] (.writeString jg (str t))))
+
+(defroutes api-routes
+  (context "/api" []
+           (GET "/ping" [] (r/response {:pong (tl/local-now)})))
+  (route/not-found "NOT FOUND"))
+
+(def app
+  (-> api-routes
+    (wrap-json-response)))
