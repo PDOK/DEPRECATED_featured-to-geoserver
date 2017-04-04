8e6edece8687ab0187636d65d0af5ae4361a5e94
Reijer Copier <Reijer.Copier@kadaster.nl>
2017-04-04 11:18:01 +0200

    Move stuff from api to core

--- src/main/clojure/pdok/featured_to_geoserver/api.clj
+++ src/main/clojure/pdok/featured_to_geoserver/api.clj
@@ -9,10 +9,9 @@
      [clojure.java.io :as io]
      [compojure.core :refer :all]
      [compojure.route :as route]
-     [org.httpkit.client :as http]
-     [cheshire.core :as json]
      [pdok.featured-to-geoserver.util :refer :all]
      [pdok.featured-to-geoserver.result :refer :all]
+     [pdok.featured-to-geoserver.core :as core]
      [pdok.featured-to-geoserver.config :as config]
      [pdok.featured-to-geoserver.database :as database]
      [pdok.featured-to-geoserver.changelog :as changelog]
@@ -60,58 +59,6 @@
            (POST "/process" [] process))
   (route/not-found "NOT FOUND"))
 
-(defn- no-close [is]
-  (proxy [java.io.FilterInputStream] [is]
-    (close [])))
-
-(defn- single-zip-entry [file]
-  (let [^java.util.zip.ZipInputStream zip (-> file (io/input-stream) (java.util.zip.ZipInputStream.))]
-    (assert (.getNextEntry zip) "Zip is empty")
-    (proxy [java.io.BufferedReader] [(io/reader (no-close zip))]
-      (close []
-        (assert (not (.getNextEntry zip)) "More than one entry in zip")
-        (.close zip)))))
-
-(defn- read-file [file format]
-  (if (= "plain" format)
-    (io/reader file)
-    (single-zip-entry file)))
-
-(defn- execute-process-request [^java.sql.Connection c request]
-  (async/go
-    (try
-      (with-open [rdr (read-file (:file request) (:format request))]
-        (let [[value error] 
-              (->> rdr 
-                (line-seq) 
-                (changelog/read-changelog)
-                (map-result 
-                  #(processor/process 
-                     (database/->DefaultTransaction c)
-                     (database/fetch-related-tables c)
-                     100
-                     %))
-                (unwrap-result))]
-          (cond 
-            error error
-            value (async/<! value))))
-      (catch Throwable t
-        (log/error t "Couldn't execute request")
-        {:failure {:exceptions (list (exception-to-string t))}}))))
-
-(defn- execute-callback [url body]
-  (try
-    (http/post 
-      url 
-      {:body (json/generate-string body)
-       :headers {"Content-Type" "application/json"}}
-      (fn [{status :status}]
-        (if (= status 200)
-          (log/info (str "Callback succeeded, url: " url))
-          (log/error (str "Callback failed, url: " url " http-status: " status)))))
-    (catch Throwable t 
-      (log/error t (str "Callback error, url: " url)))))
-
 (defn destroy! []
   (log/info "Terminating workers")
   (async/close! process-channel)
@@ -120,42 +67,11 @@
   (log/info "Application terminated"))
 
 (defn init! []
-  (doseq [worker (range config/n-workers)]
-    (async/go
-      (try
-        (with-open [^java.sql.Connection c (database/connect 
-                                             config/db
-                                             (str "Featured to GeoServer - worker " worker))]
-          (loop []            
-            (when-let [request (async/<! process-channel)]
-                (if (.isValid c 5)                  
-                  (do
-                    (log/info (str "Changelog processing started " request))
-                    (let [result (async/<! (execute-process-request c request))
-                          response {:result result :request request :worker worker}]
-                      (log/info (str "Changelog processed " response))
-                      (when (:failure result)
-                        (do
-                          (log/error "Failure while processing request -> perform rollback")
-                          (.rollback c)))
-                      (when-let [callback (:callback request)]
-                        (execute-callback callback response)))
-                    (recur))
-                  (do
-                    (let [msg "Database connection is no longer valid"]
-                      (log/error msg)
-                      (when-let [callback (:callback request)]
-                        (execute-callback 
-                          callback 
-                          {:result {:failure {:exceptions (list msg)}} 
-                           :request request 
-                           :worker worker})))
-                    (async/>! terminate-channel worker)
-                    (System/exit 1))))))
-        (catch Throwable t
-          (log/error t "Couldn't initialize worker")))
-      (async/>! terminate-channel worker)))
-  (log/info "Workers initialized"))
+  (core/create-workers 
+    config/n-workers
+    config/db
+    process-channel
+    terminate-channel))
 
 (def app
   (-> api-routes
