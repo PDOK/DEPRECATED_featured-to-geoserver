3038942e20ee9671c41139023cfc6838916369b9
Bas Duineveld <Bas.Duineveld@kadaster.nl>
2017-08-22 13:52:28 +0200

    Provide default database credentials

--- src/main/clojure/pdok/featured_to_geoserver/config.clj
+++ src/main/clojure/pdok/featured_to_geoserver/config.clj
@@ -11,13 +11,13 @@
 (defn queue-length []
   (or (as-int (env :queue-length)) 20))
 
-(defn- not-nil-env [key]
-  (let [value (env key)]
+(defn- not-nil-env [key default]
+  (let [value (or (env key) default)]
     (when (not value)
       (throw (IllegalArgumentException. (str "Environment variable missing: " key))))
     value))
 
 (defn db []
-  {:url (not-nil-env :database-url)
-   :user (not-nil-env :database-user)
-   :password (not-nil-env :database-password)})
+  {:url (not-nil-env :database-url "//localhost:5432/pdok")
+   :user (not-nil-env :database-user "postgres")
+   :password (not-nil-env :database-password "postgres")})
