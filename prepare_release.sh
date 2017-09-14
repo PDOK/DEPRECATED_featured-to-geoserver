e17940e5e11ce66bf4cf6aaf4fb2cb6d2541b214
lemkeh <hans.lemkes@kadaster.nl>
2017-09-14 12:56:45 +0200

    - Fixed the prepare_release_script.sh - Fixed the files that are accidentally committed with release 0.9.9

--- prepare_release.sh
+++ prepare_release.sh
@@ -4,19 +4,22 @@ set -e
 
 echo "Current version  $(cat VERSION)"
 
-printf "Release version? "
-read release_version
+if git diff-index --quiet HEAD --; then
+    printf "Release version? "
+    read release_version
 
-printf $release_version > VERSION
-git add VERSION
-git commit -q -m "Prepare release $release_version"
-git tag -a featured-to-geoserver-$release_version -m "Release $release_version"
+    printf $release_version > VERSION
+    git add VERSION
+    git commit -q -m "Prepare release $release_version"
+    git tag -a featured-to-geoserver-$release_version -m "Release $release_version"
 
-printf "New development version? "
-read dev_version
+    printf "New development version? "
+    read dev_version
 
-printf $dev_version > VERSION
-git add VERSION
-git commit -q -m "New release cycle $dev_version [ci skip]"
-
-git push --follow-tags
+    printf $dev_version > VERSION
+    git add VERSION
+    git commit -q -m "New release cycle $dev_version [ci skip]"
+    git push --follow-tags
+else
+    echo "There are some uncommitted files in your index, please remove or commit them first!"
+fi
